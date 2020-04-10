1a4fc17a72e650486e17ec27aaf7ba65a3c5bb5d
Sudhanva Hebbale <62728066+sudhanvaghebbale@users.noreply.github.com>
2020-04-09 22:40:24 -0700

    Added and/or operators

--- src/Grammar/projectDCG.pl
+++ src/Grammar/projectDCG.pl
@@ -1,7 +1,7 @@
-
 %----------------------------------- Main Section ----------------------------------------
 program --> [begin], block, [end].
 block --> ['{'], declarationList, [ ; ],  commandList,  [ '}' ].
+
 %---------------------------------- Declaration Section ---------------------------------------
 declarationList --> declaration, [ ; ], declarationList.
 declarationList --> declaration.
@@ -12,15 +12,18 @@ identifier -->  [X], {atom(X)}.
 integer --> [X], {integer(X)}.
 float --> [X], {float(X)}.
 bool --> [true] ; [false].
+
 %------------------------------------------------ Strings -------------------------------------------
-string--> [“], identifier, string, [“].
+string--> [â€œ], identifier, string, [â€œ].
 string--> [ ].
+
 %--------------------------------------------- Functions --------------------------------------------
 functionDeclaration --> [def],  returnType, identifier, [ '(' ], parameterList, [ ')' ], [ '{' ], commandList, [ ; ], [return], expression, [ ; ], [ '}' ].
 returnType --> dataType ; [void].
 parameterList --> parameters, parameter ; [ ].
 parameters --> parameter, parameters ; [ ].
 parameter --> dataType, identifier.
+
 %--------------------------------------------- Command Section  -------------------------------------
 commandList --> command,  [ ; ],  commandList.
 commandList  --> command.
@@ -37,6 +40,7 @@ command --> funCall.
 command --> identifier, [=], funCall.
 command --> printStatement.
 command --> boolean, [ ?],  expression, [ : ], expression.
+
 %--------------------------------------------- Expression Section  -------------------------------------
 expression --> expression, [+], term.
 expression --> expression, [-], term.
@@ -54,12 +58,14 @@ value --> stringOps.
 value --> listIdentifier.
 value --> dictionaryIdentifier.
 value --> string.
+
 %--------------------------------------------- List Section  ---------------------------------------------------
 list --> [ '[' ], identifierList, [ ']' ].
 identifierList --> listValues, element ; [ ].
 listValues --> element, listValues ; [ ].
 element --> integer ; float ; string.
 listIdentifier --> identifier, [ '[' ], integer, [ ']' ].
+
 %--------------------------------------------- Dictionary Section  --------------------------------------------
 dictionary --> ['{'], dictionaryItems, ['}'].
 dictionaryItems --> dictionaryValues, dictionaryElement ; [ ].
@@ -67,6 +73,7 @@ dictionaryValues --> dictionaryElement, dictionaryValues ;[ ].
 dictionaryElement --> string, [:], dictionaryValue.
 dictionaryValue --> integer ; float ; string.
 dictionaryIdentifier --> identifier, ['['], string, [']'].
+
 %--------------------------------------------- Operations Section  -------------------------------------
 numberOps --> typeCast ; increment ;decrement.
 increment --> identifier, [+], [+].
@@ -79,8 +86,11 @@ concatString --> [concat], ['('], identifier, identifier, [')'].
 revString --> [rev], ['('], string, [')'].
 splitString --> [split], ['('], string, integer.
 stringLength --> [len], ['('], string, [')'].
+
 %-------------------------------------- Conditional and Loop Section  -------------------------------------
 boolean --> bool.
+boolean --> expression, [and], expression.
+boolean --> expression, [or], expression.
 boolean --> expression, [==], expression.
 boolean --> [not], boolean.
 boolean --> expression, [<], expression.
@@ -88,15 +98,13 @@ boolean --> expression, [>], expression.
 boolean --> expression, [<=], expression.
 boolean --> expression, [>=], expression.
 boolean --> expression, ['!='], expression.
+
 %--------------------------------------------- Function Call  --------------------------------------------
 funCall --> identifier, ['('], callParameterList, [')'].
 callParameterList --> callParameters, callParameter ; [ ].
 callParameters --> callParameter, callParameters ; [ ].
 callParameter --> identifier.
+
 %--------------------------------------------- Print Statement  --------------------------------------------
 printStatement --> [print], ['('], item, [')'].
 item --> expression; string.
-
-
-
-
