fdb9cf9473a8c0f5dd35e509d4c0ab4885ad21b0
Jubanjan Dhar <jubanjan1@gmail.com>
2020-04-21 02:07:45 -0700

    Comment Friendly
    Lexer can now identify comments and ignore them.

--- Lexer/lexer.py
+++ Lexer/lexer.py
@@ -1,9 +1,10 @@
 from nltk.tokenize import word_tokenize
+import re
 
 # Need the below lines if running for the first time
 
-import nltk
-nltk.download('punkt')
+# import nltk
+# nltk.download('punkt')
 
 class Lexer():
     def __init__(self,FileName):
@@ -14,8 +15,13 @@ class Lexer():
 
     def readFile(self):
         with open (self.file, 'r') as f:
-            data = f.readlines()
+            data = self.removeComments(f.read())
+        
         return data
+
+
+    def returnData(self):
+        return self.data
 
     def tokenize(self):
         for ls in self.data:
@@ -23,14 +29,37 @@ class Lexer():
         
         return word_tokenize(self.strg)
 
+    def removeComments(self,data):
+        
+        def replacer(match):
+            s = match.group(0)
+            if s.startswith('/'):
+                return "\n" * s.count( "\n" ) # note: a space and not an empty string
+            else:
+                return s
+
+        pattern = re.compile(r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',re.DOTALL | re.MULTILINE)
+
+        return re.sub(pattern, replacer, data) 
+
 
 
 
 if __name__ =='__main__':
     res_ =  Lexer('./script.txt').tokenize()
+    # check = ['begin', 'num', 'x', '=', '2', ';', 'num', 'r', ';', 'num', 'u', '=', '0', ';', 'num', 'z', ';', 'num', 'v', ';', 'str', 's', '=', '"', 'sudhanva', '"', ';', 'num', 'y', '=', '0', ';', 'for', 'i', 'in', 'range', '(', '1', ',' , '5', ')', '{', 'x', '=', 'x', '+', '1', ';', 'y', '=', 'y', '+', '1', '}', ';', 'while', '(', 'not', 'u', '==', '3', ')', '{', 'z', '=', 'x', '*', '2', ';', 'u', '=', 'u', '+', '1','}',';', 'if', '(', 'z', '>', 'x', ')', '{', 'v', '=', '1',';', '}', 'else', '{', 'v', '=', '0',';', '}', ';', 'x', '<', '3', '?', 'r', '=', '0', ':', 'r', '=' , '1', ';', 'end']
+    # print(len(check))
+    for j,i in enumerate(res_):
+        if i =='``' or i =="''":
+            res_[j] = '"'
+    
+    strg = ''
+    for i in res_:
+        strg += i+','+'\n'
+
     with open('tokens.txt','w') as f:
-        f.write(str(res_))
-    print('Tokens compiled to tokens.txt')
+        f.write(strg)
+ 
 
     
     
--- Lexer/readme.md
+++ Lexer/readme.md
@@ -2,7 +2,11 @@
 
 
 ### TODO:
-- [ ] Lexer should be able to handle syntax errors.
+- [x] Lexer should be able to handle syntax errors.
+- [x] Remove comments from input source.
+- [x] Accomodated String declarations in source.
+- [x] Output file readable by prolog Script.
 - [ ] To make the lexer.py take filenames as arguments from shell to compile tokens on-the-go. 
 - [ ] Execute Multiple files at once?
+- [ ] Tokenizer doesn't tokenize ,.+-^|/\= so, need spaces between characters when declaring them. 
 
