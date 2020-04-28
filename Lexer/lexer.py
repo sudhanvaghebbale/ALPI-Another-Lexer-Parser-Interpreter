df0d5dc7fd87cd5298f0365a6a090b7558ef003a
Jubanjan Dhar <jubanjan1@gmail.com>
2020-04-27 22:18:04 -0700

    small bug
    fixed list[i]++

--- Lexer/lexer.py
+++ Lexer/lexer.py
@@ -126,19 +126,24 @@ if __name__ =='__main__':
     res_ =  Lexer('/Users/jubanjanmacbook/PycharmProjects/502_Project/SER502-Spring2020-Team26/Lexer/script.txt').tokenize()
     # check = ['begin', 'num', 'x', '=', '2', ';', 'num', 'r', ';', 'num', 'u', '=', '0', ';', 'num', 'z', ';', 'num', 'v', ';', 'str', 's', '=', '"', 'sudhanva', '"', ';', 'num', 'y', '=', '0', ';', 'for', 'i', 'in', 'range', '(', '1', ',' , '5', ')', '{', 'x', '=', 'x', '+', '1', ';', 'y', '=', 'y', '+', '1', '}', ';', 'while', '(', 'not', 'u', '==', '3', ')', '{', 'z', '=', 'x', '*', '2', ';', 'u', '=', 'u', '+', '1','}',';', 'if', '(', 'z', '>', 'x', ')', '{', 'v', '=', '1',';', '}', 'else', '{', 'v', '=', '0',';', '}', ';', 'x', '<', '3', '?', 'r', '=', '0', ':', 'r', '=' , '1', ';', 'end']
     ''' handle quotes'''
+
+
     for j,i in enumerate(res_):
         if i =='``' or i =="''":
             res_[j] = '"'
 
+
+
     # tokenize increment operation, since tokenizer tokenizes decrement only
-    pos = [pos for pos, char in enumerate(res_) if '++' in char]
-    if len(pos) != 0:
+    pos = [pos for pos, char in enumerate(res_) if '++' in char and len(char)>2 ]
+    if len(pos) > 0:
         x_ = range(len(pos))
         pos_ = list( map(add, pos, x_) )
         for i in pos_:
             res_[i:i] = [res_[i].split('++')[0],'++']
             del res_[i+2]
-
+            
+    
     # tokenize i==1,i ==1, i== 1
     pos_eq1 = [pos for pos, char in enumerate(res_) if '==' in char]
     if len(pos_eq1) != 0:
